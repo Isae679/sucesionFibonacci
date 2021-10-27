87f94b8b1bcae522bc01bedbd275fb44eb5cb3fc
isabel <blancaisabel4321@gmail.com>
2021-10-27 08:28:46 -0400

    Maximo de un vector

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -7,16 +7,16 @@ public class Fibonacci {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("numero de elementos: ");
+       // System.out.println("numero de elementos: ");
         int limite =sc.nextInt();
         sc.close();
-        System.out.println("sucesion fibonacci:");
+      //  System.out.println("sucesion fibonacci:");
         for (int i = 0; i < limite ; i++) {
             System.out.println(fibonacci(i) );
 
 
         }
-        System.out.println("enecimo termino de la sucesion fibonacci: ");
+        //System.out.println("enecimo termino de la sucesion fibonacci: ");
 
     }
 
@@ -33,6 +33,37 @@ public class Fibonacci {
 
         }
     }
+
+/*
+ Metoodo recursivo para calcular el maximo valor de un vector
+ vector = {2, 9, 6, 7, 6, 3, 4, 3}
+*/
+
+private static int maximoVector(int[] vector, int indice, int maximo){
+
+    if (indice == vector.length -1){//caso base
+        if (vector [indice] > maximo) {
+            maximo = vector[indice];
+
+        }
+
+    }else{//llamada recursividad
+        if (vector [indice] > maximo) {
+            maximo = vector[indice];
+        }
+        maximo = maximoVector(vector, indice + 1, maximo);
+    }
+
+
+    return  maximo;
+}
+
+/*Metodo auxiliar que llama al metodo recursivo*/
+public static int maximoVector(int[] vector){
+
+    return maximoVector(vector, 0, 0);
+}
+
 
     }
 
