ab9fea5df2f2e0bc5271d453820039317033e537
Blanca Isabel Esquibel Loayza <blancaisabel4321@gmail.com>
2021-11-17 05:42:44 -0400

    Indice de un vector

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -65,6 +65,34 @@ public static int maximoVector(int[] vector){
 }
 
 
+/////Indice de un vector
+
+    public static int indiceVector(int[] vector, int indice){
+    int contador = 0 ;
+    if (indice == vector.length - 1){//caso base
+        if(indice == vector[indice])
+            contador++;
+
+    }else {//llamada recursiva
+
+        if (indice == vector[indice]){
+            contador++;
+        }
+        contador = contador + indiceVector(vector, indice + 1);
+
+    }
+    return contador;
+
+    }
+    //metodo auxiliar
+    public static boolean indiceVector(int[] vector ){
+    return indiceVector(vector, 0) > 0;
+
+    }
+
+
+
+
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-       int[] vector = {2, 9, 6, 7, 6, 3, 4, 3};
+     /*  int[] vector = {2, 9, 6, 7, 6, 3, 4, 3};
         System.out.println(Fibonacci.maximoVector(vector));
+
+      */
+        //indice vector
+        int[] vector = {10, 8, 6, 4, 3, 12, 1, 0};
+        System.out.println(Fibonacci.indiceVector(vector, 0));
 
 
     }
